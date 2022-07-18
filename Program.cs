91272622bf48387b05e3836015f668f38fa73ef6
Jesus <je-farias@hotmail.com>
2022-07-18 10:34:53 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Projeto_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Bem vindo ao tabuapp!");
+            WriteLine("Insira o valor que deseja multiplicar: ");
+            int valor = int.Parse(ReadLine());
+            
+            int i = 0;
+            while (i <= 10 ){
+                int multiplicacao = valor * i;
+                WriteLine($"O resultado de {valor} x {i} é: {multiplicacao}");
+                i++;
+            }
+        }
+    }
+}
